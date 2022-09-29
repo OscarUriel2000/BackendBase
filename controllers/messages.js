78bf5dc0382424dab5d31db67fd6d8265c858cb4
Oscar Uriel Alcocer Delfin <isc18350287@gmail.com>
2022-09-29 10:16:45 -0500

    Parametros

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,12 +1,24 @@
 const { request, response } = require("express");
 const rootMessages = (req=request, res=response) => {
-    res.status(404).json ({msg:'Mensajes'});
+    const {texto1, texto2} = req.query
+    // if (!texto1 || !texto2) {
+    //     res.status(400).json({
+    //         msg: "No se han enviado los parametros necessarios, este EndPoint ocupa los parametros 'texto1' y 'texto2'"
+    //     })
+    // }
+    if (!texto1){
+        res.status(400).json({msg: "Falta el parametro 'texto1'"})
+    }
+    if (!texto2){
+        res.status(400).json({msg: "Falta el parametro 'texto2'"})
+    }
+    res.status(200).json ({msg: texto1 + ' ' + texto2});
 }
 const hiMessages =  (req=request, res=response) => {
     res.status(406).json ({msg:'Hola mundo'});
 }
 const byeMessages = (req=request, res=response) => {
-    res.status(406).json ({msg:'Adios mundo'});
+    res.status(418).json ({msg:'Adios mundo'});
 }
 const postMessages = (req=request, res=response) => {
     res.status(407).json ({msg:'Mensaje Post'});
