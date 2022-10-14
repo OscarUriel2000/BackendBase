1615a39b5b51e311a3db443ed85349201fba0683
Oscar Uriel Alcocer Delfin <isc18350287@gmail.com>
2022-10-14 10:13:41 -0500

    usuario

---
+++ controllers/usuarios.js
@@ -0,0 +1,6 @@
+const {request, response } = require ("express");
+const getUsers = (req = request, res = response) => {
+    console.log("funcion getUsers")
+    res.json({msg: "Funcion getUsers"})
+}
+module.exports = {getUsers}
--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-const express = require('express');
-const menssagesRouter = require('./routes/messages')
+const express = require('express')
+const messagesRouter = require('./routes/messages')
+const usuariosRouter = require('./routes/usuarios')
 const cors = require ("cors")
 class Server {
     constructor(){
         this.app = express()
-        this.port = process.env.PORT
         this.paths = {
             messages: "/api/v1/messages",
+            usuarios: "/api/v1/usuarios"
         }
         this.middlewares()
         this.routes()
     }
     routes(){
-      this.app.use(this.paths.messages, menssagesRouter) 
+        this.app.use(this.paths.messages, messagesRouter),
+        this.app.use(this.paths.usuarios, usuariosRouter)
     }
     middlewares(){
         this.app.use(cors())
         this.app.use(express.json())
    }
     listen(){
-        this.app.listen(this.port, () => {
-            console.log('servidor corriendo en el puerto ', this.port);
+        this.app.listen(process.env.PORT, () => {
+            console.log(process.env.PORT);
         })
     }
 }
