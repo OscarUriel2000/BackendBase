e98097271743440d8a6282e132abf2d11b405d9c
Oscar Uriel Alcocer Delfin <isc18350287@gmail.com>
2022-10-14 11:01:59 -0500

    Conexion a la BD

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,6 +1,22 @@
 const {request, response } = require ("express");
-const getUsers = (req = request, res = response) => {
-    console.log("funcion getUsers")
-    res.json({msg: "Funcion getUsers"})
+const pool = require ("../db/connection")
+const getUsers = async (req = request, res = response) => {
+    let conn;
+    try {
+        conn = await pool.getConnection()
+        const users = await conn.query("SELECT * FROM Usuarios", (error) => {throw new Error(error)})
+        if (!users){
+            res.status(404).json({msg: "No se encontraron registros"})
+            return
+        }         
+        res.json({users}) 
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error})
+    } finally{
+        if (conn){
+            conn.end()
+        }
+    }
 }
 module.exports = {getUsers}
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -5,4 +5,4 @@ const router = Router()
 //http://localhost:4000/api/v1/usuarios
 
 router.get("/", getUsers)
-module.exports = router
+module.exports = router 
