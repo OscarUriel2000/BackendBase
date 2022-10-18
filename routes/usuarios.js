c6730d81952d7a4d29a1571926c45324be164109
Oscar Uriel Alcocer Delfin <isc18350287@gmail.com>
2022-10-18 11:01:39 -0500

    ID

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,8 +1,9 @@
 const {Router} = require("express")
-const {getUsers} = require("../controllers/usuarios")
+const {getUsers, getUserByID} = require("../controllers/usuarios")
 const router = Router()
 
 //http://localhost:4000/api/v1/usuarios
 
 router.get("/", getUsers)
+router.get("/id/:id", getUserByID)
 module.exports = router 
