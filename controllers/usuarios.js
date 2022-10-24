94fe552e39e407af41ef8fdbd761794da4b3fe63
Oscar Uriel Alcocer Delfin <isc18350287@gmail.com>
2022-10-24 11:41:26 -0500

    Eliminar usuarios

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -39,4 +39,24 @@ const getUserByID = async (req = request, res = response) => {
         }
     }
 }
-module.exports = {getUsers, getUserByID}
+const deleteUserByID = async (req = request, res = response) => {
+    const {id} = req.query
+    let conn;
+    try {
+        conn = await pool.getConnection()
+    const {affectedRows} = await conn.query(`UPDATE Usuarios SET Activo = 'N' WHERE ID = ${id}`, (error) => {throw new Error(error)})
+    if (affectedRows === 0){
+            res.status(404).json({msg: `No se pudo eliminar el registro con el ID ${id}`})
+            return
+        }         
+        res.json({msg: `El usuario con ID ${id} se elimino correctamente.`}) 
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error})
+    } finally{
+        if (conn){
+            conn.end()
+        }
+    }
+}
+module.exports = {getUsers, getUserByID, deleteUserByID}
